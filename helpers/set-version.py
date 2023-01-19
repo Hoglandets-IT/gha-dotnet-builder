48e89fbb142fa26705d7580d11d5208a2e148b2f
scheibling <24367830+scheibling@users.noreply.github.com>
2023-01-19 15:36:40 +0100

    Read version from file instead of input

--- helpers/set-version.py
+++ helpers/set-version.py
@@ -6,25 +6,31 @@ params = {
     "suffix": None
 }
 
-alen = len(sys.argv)
+try:
+    alen = len(sys.argv)
 
-if alen > 1:
-    params['version'] = sys.argv[1]
-    if alen > 2:
-        params['increment'] = sys.argv[2]
-        if alen > 3:
-            params['suffix'] = sys.argv[3]
+    if alen > 1:
+        with open(sys.argv[1], 'r') as f:
+            params['version'] = f.read().strip()
+        if alen > 2:
+            params['increment'] = sys.argv[2]
+            if alen > 3:
+                params['suffix'] = sys.argv[3]
 
-split = params['version'].split('.')
-if params['increment'] != '':
-    if params['increment'] == 'major':
-        split[0] = str(int(split[0]) + 1)
-        split[1] = '0'
-        split[2] = '0'
-    elif params['increment'] == 'minor':
-        split[1] = str(int(split[1]) + 1)
-        split[2] = '0'
-    elif params['increment'] == 'patch':
-        split[2] = str(int(split[2]) + 1)
+    split = params['version'].split('.')
 
+    if params['increment'] != '':
+        if params['increment'] == 'major':
+            split[0] = str(int(split[0]) + 1)
+            split[1] = '0'
+            split[2] = '0'
+        elif params['increment'] == 'minor':
+            split[1] = str(int(split[1]) + 1)
+            split[2] = '0'
+        elif params['increment'] == 'patch':
+            split[2] = str(int(split[2]) + 1)
+except:
+    print('Error: Invalid arguments')
+    print('Ver: ' + params['version'])
+    print('Split: ' + ','.join(split))
 print('.'.join(split) + (('-' + params['suffix']) if params['suffix'] not in ["", " ", None, False] else ''))
